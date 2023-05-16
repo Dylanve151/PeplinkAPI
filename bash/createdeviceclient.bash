34c08ddd71dc4ab0c4c84fab03954e0cce011c6c
Dylanve151 <Dylanvanelst@outlook.com>
2023-05-16 14:57:59 +0200

    changed some stuff

--- bash/createdeviceclient.bash
+++ bash/createdeviceclient.bash
@@ -2,10 +2,23 @@
 
 export $(cat .env)
 
-admin_login='{"username":"'$admin_user'","password":"'$admin_pass'"}'
+if [[ $client_name == "" ]] || [[ $client_name == $null ]]
+then
+    client_name='APIexample'
+fi
 
-loginresponse=$(curl -k -c /tmp/cookies.txt -H "Content-Type: application/json" -X POST -d $admin_login ${server_prefix}/api/login)
+admin_login_url=${server_prefix}'/api/login'
+client_auth_url=${server_prefix}'/api/auth.client'
 
-clientresponse=$(curl -k -b /tmp/cookies.txt -H "Content-Type: application/json" -X POST -d '{"action":"add","name":"Client2","scope":"api"}' ${server_prefix}/api/auth.client)
+admin_login_params='{"username":"'$admin_user'","password":"'$admin_pass'"}'
+loginresponse=$(curl -ks -c /tmp/cookies.txt -H "Content-Type: application/json" -X POST -d $admin_login_params $admin_login_url)
 
-echo $clientresponse | jq -r .response
+client_auth_params='{"action":"add","name":"'$client_name'","scope":"api"}'
+clientresponse=$(curl -ks -b /tmp/cookies.txt -H "Content-Type: application/json" -X POST -d $client_auth_params $client_auth_url)
+
+if [[ $(echo $clientresponse | jq -r .stat) == "ok" ]]
+then
+    echo $clientresponse | jq -r .response
+else
+    echo $clientresponse | jq -r .
+fi
--- python/createdeviceclient.py
+++ python/createdeviceclient.py
@@ -11,17 +11,24 @@ load_dotenv()
 admin_user = os.getenv('admin_user')
 admin_pass = os.getenv('admin_pass')
 server_prefix = os.getenv('server_prefix')
+client_name = os.getenv('client_name')
+
+if client_name == None:
+    client_name = 'APIexample'
 
 verify_ssl_cert = False
 
 admin_login_url = server_prefix + '/api/login'
-client_create_url = server_prefix + '/api/auth.client'
+client_auth_url = server_prefix + '/api/auth.client'
 
 admin_login_params = '?username=' + admin_user + '&password=' + admin_pass
 loginresponse = requests.post((admin_login_url + admin_login_params), verify = verify_ssl_cert, headers = {'Content-type': 'application/x-www-form-urlencoded'})
 
-client_create_params = '?action=' + 'add' + '&name=' + 'Client2' + '&scope=' + 'api'
-tmpresponse = requests.post((client_create_url + client_create_params), verify = verify_ssl_cert, headers = {'Content-type': 'application/x-www-form-urlencoded'}, cookies = loginresponse.cookies)
+client_auth_params = '?action=' + 'add' + '&name=' + client_name + '&scope=' + 'api'
+clientresponse = requests.post((client_auth_url + client_auth_params), verify = verify_ssl_cert, headers = {'Content-type': 'application/x-www-form-urlencoded'}, cookies = loginresponse.cookies)
 
-rqjson = json.loads(tmpresponse.text)
-rqjson["response"]
+rqjson = json.loads(clientresponse.text)
+if rqjson["stat"] == 'ok':
+    print(rqjson["response"])
+else:
+    print(rqjson)
